d80b322c21359fcf688eed7d5f3c5fe54cc54b4a
ThanhNDSE <thanh06062k5@gmail.com>
2026-06-09 20:59:08 +0700

    change nguyen duc thanh to tran quoc an

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -9,5 +9,5 @@ public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-// nguyen duc thanh
+// tran quoc an
 }
