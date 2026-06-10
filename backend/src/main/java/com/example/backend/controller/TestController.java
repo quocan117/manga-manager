9edfba5c33deede7d3e054f83c50adec207ab030
may phu <tranquocan11072005@gmail.com>
2026-06-10 11:04:24 +0700

    Initial commit

---
+++ backend/src/main/java/com/example/backend/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.backend.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*") // CỰC KỲ QUAN TRỌNG: Cho phép Frontend khác cổng (3000 hoặc 5173) được phép gọi vào
+public class TestController {
+
+    @GetMapping("/ping")
+    public String pingTest() {
+        return "✅ Kết nối thành công! Backend Spring Boot đang chạy mượt mà!";
+    }
+}
