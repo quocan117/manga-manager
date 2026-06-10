7b01527ab44bd026f2bf9ba7b6f97b93d2554bf6
ThanhND-SE190751 <thanh06062k5@gmail.com>
2026-06-10 11:34:28 +0700

    lần 2

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -9,5 +9,5 @@ public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-// test delele
+// test delele lần 2
 }
