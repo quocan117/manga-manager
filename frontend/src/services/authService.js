fab52e2fdb500aa20495c300b77003e2612d12d3
TranQuocAn <quocan1919@gmail.com>
2026-07-13 18:17:23 +0700

    Add Google login as sign-in option

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,9 +1,16 @@
 import api from "./api";
+
 export const login = async (form) => {
-    const response = await api.post("/auth/login", form);
-    return response.data;
+  const response = await api.post("/auth/login", form);
+  return response.data;
 };
+
 export const register = async (registerData) => {
-    const response = await api.post("/auth/register", registerData);
-    return response.data;
+  const response = await api.post("/auth/register", registerData);
+  return response.data;
+};
+
+export const googleLogin = async (idToken) => {
+  const response = await api.post("/auth/google-login", { idToken });
+  return response.data;
 };
